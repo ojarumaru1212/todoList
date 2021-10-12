deb6be87aae4c5d5e8a96532a593bd2abd5ce4bd
ojarumaru1212 <yoshiaki.ojarumaru@gmail.com>
2021-10-12 23:07:58 +0900

    ログイン処理作成開始

---
+++ app/src/main/java/com/todolist/api/common/constant/ParameterConst.java
@@ -0,0 +1,5 @@
+package com.todolist.api.common.constant;
+
+public class ParameterConst {
+
+}
---
+++ app/src/main/java/com/todolist/api/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.todolist.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+	@GetMapping("/login")
+	public String greeting() {
+//		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		return "test";
+	}
+}
