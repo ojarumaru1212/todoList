9625155b7d56f1eb0a35ee69d110fe0a4c37d1f5
ojarumaru1212 <yoshiaki.ojarumaru@gmail.com>
2021-10-13 23:05:03 +0900

    エンドポイントの定数対応

--- app/src/main/java/com/todolist/api/common/constant/ParameterConst.java
+++ app/src/main/java/com/todolist/api/common/constant/ParameterConst.java
@@ -1,5 +1,5 @@
 package com.todolist.api.common.constant;
 
 public class ParameterConst {
-
+	public static final String PARAM_LOGIN = "/login";
 }
--- app/src/main/java/com/todolist/api/controller/LoginController.java
+++ app/src/main/java/com/todolist/api/controller/LoginController.java
@@ -3,10 +3,12 @@ package com.todolist.api.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.todolist.api.common.constant.ParameterConst;
+
 @RestController
 public class LoginController {
 
-	@GetMapping("/login")
+	@GetMapping(ParameterConst.PARAM_LOGIN)
 	public String greeting() {
 //		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 		return "test";
